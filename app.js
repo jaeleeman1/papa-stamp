97aa073312bb485104d8fbb59f63bb15ea3d6da6
yunchae <ck2204@naver.com>
2017-01-10 16:10:21 +0900

    FoodDetail -> foodDetail

--- app.js
+++ app.js
@@ -7,7 +7,7 @@ var bodyParser = require('body-parser');
 
 var index = require('./routes/index');
 var users = require('./routes/users');
-var FoodDetail = require('./routes/FoodDetail');
+var foodDetail = require('./routes/foodDetail');
 
 // app express
 var app = express( );
@@ -26,7 +26,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', index);
 app.use('/users', users);
-app.use('/FoodDetail', FoodDetail);
+app.use('/foodDetail', foodDetail);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
