2b1fea966c5f704a16b7d10d3e1ae018715ecafa
yunchae <ck2204@naver.com>
2017-01-11 19:35:09 +0900

    app.js -> food로 변경

--- app.js
+++ app.js
@@ -7,7 +7,7 @@ var bodyParser = require('body-parser');
 
 var index = require('./routes/index');
 var users = require('./routes/users');
-var restaurantDetail = require('./routes/food');
+var food = require('./routes/food');
 
 // app express
 var app = express( );
@@ -26,7 +26,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', index);
 app.use('/users', users);
-app.use('/food', restaurantDetail);
+app.use('/food', food);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
