5a12d8b8ee642cb6c2f9585838a417d0ea65afa5
yunchae <ck2204@naver.com>
2017-01-13 15:20:14 +0900

    foodShopList 추가

--- routes/food.js
+++ routes/food.js
@@ -1,17 +1,35 @@
 /**
  * Created by yunchae.na on 2017-01-09.
  */
-
 var express = require('express');
 var router = express.Router();
 var getConnection = require('./db');
+// var baiduMap = require('baidumap');
 
-//GET shopping detail Information
+//GET foodShop List
+router.get('/shopList', function (req, res, next) {
+
+    getConnection(function (err, connection){
+        var query = 'select * from TB_FOOD_SHOP_LIST_CN';
+
+        connection.query(query, function (err, rows) {
+            if (err) {
+                //  console.error("err : " + err);
+                throw err;
+            }else{
+                   console.log("rows : " + JSON.stringify(rows));
+                   res.render('foodShopList', {rows : rows, length:rows.length});
+            }
+            connection.release();
+        })
+    });
+});
+
+
+//GET foodShop detail Information
 router.get('/shopInfo', function (req, res, next) {
-
     getConnection(function (err, connection) {
-
-        var query = 'select * from TB_FOOD_SHOP_LIST_CN A, TB_FOOD_MENU B where A.ID = B.ID and A.ID = ?';
+        var query = 'select * from TB_FOOD_SHOP_LIST_CN A, TB_FOOD_MENU B where A.FOOD_ID = B.FOOD_ID and A.FOOD_ID = ?';
         var id = req.query.id; // foodList Id
 
         connection.query(query, id, function (err, rows) {
