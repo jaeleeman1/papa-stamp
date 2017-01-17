5f5a59c715bc86c3939a4c86e92306278a629d28
felix <fromjaelee@naver.com>
2017-01-17 13:31:06 +0900

    쇼핑 리스트 화면 개발

--- routes/shopping.js
+++ routes/shopping.js
@@ -2,13 +2,8 @@ var express = require('express');
 var router = express.Router();
 var getConnection = require('../lib/db_connection');
 
-//GET foodShopMap Basic rendering
+//GET Shopping rendering
 router.get('/', function (req, res, next) {
-    res.render('shopping/shoppingList');
-});
-
-//GET foodShopList
-router.get('/shoppingList', function (req, res, next) {
     getConnection(function (err, connection){
         var query = 'select * from TB_SHOPPING_LIST';
 
@@ -18,7 +13,26 @@ router.get('/shoppingList', function (req, res, next) {
                 throw err;
             }else{
                 // console.log("foodShopList success : " + JSON.stringify(row));
-                res.send({data:row});
+                res.render('shopping/shoppingList', {data:row});
+            }
+            connection.release();
+        })
+    });
+});
+
+//GET Shopping Detail
+router.get('/shoppingDetail', function (req, res, next) {
+    getConnection(function (err, connection){
+        var query = 'select * from TB_SHOPPING_LIST as TSL where TSL.PRDCT_ID = ?';
+        var id = req.query.id; // product Id
+
+        connection.query(query, id, function (err, row) {
+            if (err) {
+                console.error("err : " + err);
+                throw err;
+            }else{
+                // console.log("foodShopList success : " + JSON.stringify(row));
+                res.render('shopping/shoppingDetail', {data:row});
             }
             connection.release();
         })
