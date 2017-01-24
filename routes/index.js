ad9ef12e6d098040bd25c2dd2a2122f965bace18
yunchae <ck2204@naver.com>
2017-01-24 13:30:38 +0900

    Merge branch 'development' of C:\Users\SDS\WebstormProjects\CouPhone_Alpha_Server with conflicts.

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,22 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('foodShopList', { title: 'Express' });
+
+    var dataString = {
+        User_Open_Id : "oH7FywN073pRVpCF0G6nMl6iI8mg",
+        Prdct :
+            [
+                {Prdct_Nm : "Coffee1", Prdct_Cnt : "8", Price:"20" },
+                {Prdct_Nm : "Coffee2", Prdct_Cnt : "4", Price:"25" },
+                {Prdct_Nm : "Coffee3", Prdct_Cnt : "5", Price:"30" }
+            ]
+    };
+
+
+    console.log('dataString', dataString.Prdct[0].P);
+
+
+  // res.render('foodShopList', { title: 'Express' });
 });
 
 module.exports = router;
