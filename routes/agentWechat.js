9e6b8a03705f69fb39e096d89b2c729229c8a77b
jaeleeman1 <fromjaelee@naver.com>
2017-01-26 00:22:50 +0900

    Update agentWechat.js

--- routes/agentWechat.js
+++ routes/agentWechat.js
@@ -36,11 +36,11 @@ router.post('/agentWechat', function(req, res, next) {
     {
 	console.log('Destination : ',req.body);
 	getConnection(function (err, connection){
-		var wechatId = req.body.wechatId; // wechat Id
+		var wechatId = req.body.wechat_id; // wechat Id
 		var destination = req.body.destination; // destination
 		var query = 'UPDATE TB_ROAD_INFO SET TRANSLATION_ADDR_CN ="'+ destination +'" WHERE USER_OPEN_ID=?';
 
-		connection.query(query, function (err, row) {
+		connection.query(query, wechatId, function (err, row) {
 			if (err) {
 				console.error("err : " + err);
 				throw err;
