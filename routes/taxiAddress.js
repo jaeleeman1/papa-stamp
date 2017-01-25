9b3f6bff56c1f1fab8412b455c825fe612c813c3
jaeleeman1 <fromjaelee@naver.com>
2017-01-26 00:45:57 +0900

    Update taxiAddress.js

--- routes/taxiAddress.js
+++ routes/taxiAddress.js
@@ -6,7 +6,7 @@ var getConnection = require('../lib/db_connection');
 router.get('/', function (req, res, next) {
     getConnection(function (err, connection) {
         var wechatId = 'jaeleeman1'//req.query.wechat_id;
-        var query = 'SELECT TRANSLATION_ADDR_CN FROM TB_ROAD_INFO WHERE  USER_OPEN_ID = ?  ORDER BY ROAD_SEQ DESC LIMIT 1';
+        var query = 'SELECT END_NM_CN, TRANSLATION_ADDR_CN FROM TB_ROAD_INFO WHERE  USER_OPEN_ID = ?  ORDER BY ROAD_SEQ DESC LIMIT 1';
 
         connection.query(query, wechatId, function (err, rows) {
             if (err) {
@@ -14,7 +14,7 @@ router.get('/', function (req, res, next) {
                 throw err;
             }else{
                 console.log("rows : " + JSON.stringify(rows));
-                res.render('taxiAddress', { rows : rows });
+                res.render('taxiAddress', {data:rows[0]});
             }
             connection.release();
         })
