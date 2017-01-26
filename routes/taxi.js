b9ba9a6a224f75d8717622600d874784016509b2
jaeleeman1 <fromjaelee@naver.com>
2017-01-26 10:14:51 +0900

    Update taxi.js

--- routes/taxi.js
+++ routes/taxi.js
@@ -98,6 +98,25 @@ router.get('/transport', function (req, res, next) {
     })
 });
 
+//Taxi target address  Information
+router.get('/address', function (req, res, next) {
+    getConnection(function (err, connection) {
+        var wechatId = 'jaeleeman1'//req.query.wechat_id;
+        var query = 'SELECT END_NM_CN, TRANSLATION_ADDR_CN FROM TB_ROAD_INFO WHERE  USER_OPEN_ID = ?  ORDER BY ROAD_SEQ DESC LIMIT 1';
+
+        connection.query(query, wechatId, function (err, rows) {
+            if (err) {
+                //  console.error("err : " + err);
+                throw err;
+            }else{
+                console.log("rows : " + JSON.stringify(rows));
+                res.render('taxiAddress', {data:rows[0]});
+            }
+            connection.release();
+        })
+    })
+});
+          
 var getDuration = function(duration) {
 
     var hour = Math.floor(duration/ (60*60));
