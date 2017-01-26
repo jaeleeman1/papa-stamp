a0d9234b5c75af2d5d489904dafbf71c587a73fc
yunchae <ck2204@naver.com>
2017-01-26 22:15:58 +0900

    agent 화면 css 수정

--- routes/agentWechat.js
+++ routes/agentWechat.js
@@ -104,8 +104,7 @@ router.post('/sendTaxiMap', function (req, res, next) {
                                                 var mapUrl = 'http://nbnl.couphone.cn:8080/taxi/transport?id=' + wechatId +'&type=driving'
                                                 var messageUrl = 'http://nbnl.couphone.cn:8080/taxi/taxiaddress?name='+ arrive.nameCn +'&address='+ translationAddrCn;  //중국어 보여주는 url
                                                 var message    =    "약 " +    duration +" "+ distance  +  "\n";
-                                                     message     +=  '도착지 : ' + arrive.nameCn + '\n'+
-                                                                     '        (' + arrive.nameKr + ')';
+                                                     message     +=  '도착지 : ' + arrive.nameCn + ' (' + arrive.nameKr + ')';
 
                                                 var contents = {
                                                                     "touser" : openId,
