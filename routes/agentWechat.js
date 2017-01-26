1a6dd9c3ce9344735494b96c12853648f1c10437
yunchae <ck2204@naver.com>
2017-01-26 11:31:21 +0900

    택시 출발지 메시지 전송

--- routes/agentWechat.js
+++ routes/agentWechat.js
@@ -116,7 +116,8 @@ router.post('/taxiDepartSend', function(req, res, next) {
             } else {
 
             	var taxiMsg = '';
-                taxiMsg+= '택시 안내 요청 \n현위치 : ' + addr;
+                taxiMsg+= '택시 안내 요청 \n현위치 : ';
+                // taxiMsg+= '택시 안내 요청 \n현위치 : ' + addr;
 
                 // console.log('openId', openId);
                 // console.log('taxiMsg', taxiMsg);
