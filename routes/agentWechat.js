b242a4669d0b2987511a540899aaa08743020506
mjmts <mjmts@naver.com>
2017-01-26 15:28:49 +0900

    agentWechat에 택시 주소 업데이트 처리 문자 전송 처리 추가

--- routes/agentWechat.js
+++ routes/agentWechat.js
@@ -26,43 +26,23 @@ router.post('/agentWechat', function(req, res, next) {
 
     if (req.body.message == "1")
     {
-        console.log('RoadAddressInsert ',req.body);
-        //wechat id로 open id 가져오기
-        getOpenId(req , res   );
-    }
-	
-	//insert destination
-    if (req.body.message == "2")
-    {
-	console.log('Destination : ',req.body);
-	getConnection(function (err, connection){
-		var wechatId = req.body.wechat_id; // wechat Id
-		var destination = req.body.destination; // destination
-		var query = 'UPDATE TB_ROAD_INFO SET TRANSLATION_ADDR_CN ="'+ destination +'" WHERE USER_OPEN_ID=?';
+        console.log(" Before!! Select   MaxSeq ");
+        SelectMaxSeq(req,res,next);
 
-		connection.query(query, wechatId, function (err, row) {
-			if (err) {
-				console.error("err : " + err);
-				throw err;
-			}else{
-				console.log("### update destination ### - wechatId : " + wechatId);
-				res.status(200).send('Send Sucess');
-			}
-			connection.release();
-		})
-	});        
     }
 
     //data send
-    if (req.body.message == "3")
+    if (req.body.message == "2")
     {
         console.log('SendMessage ',req.body);
         SendMessage(req, res, next);
     }
 
-    if (req.body.message == "4")
+
+    if (req.body.message == "3")
     {
         console.log('send food rich message ' );
+
         // 리치 메세지를 보내기 위해서 req.body에 추가 데이타를 적재함
         req.body.UserOpenId = "oiduvwC2PiPzvfPt3e4txGT39gOk";
         req.body.StartPos = "Chinese Street";
@@ -73,7 +53,8 @@ router.post('/agentWechat', function(req, res, next) {
         req.body.Distance = "400(memter)"
         req.body.ImageUrl = "Http://naver.com"
 
-        api.sender.foodResSend( req , res, next)
+
+        api.foodResultSender.foodResSend( req , res, next)
         {
             console.log( res.statusCode );
             if ( res.statusCode != 200) {
@@ -195,6 +176,11 @@ function getToken(openId,sendMessage) {
     var appID = 'wx87ac1cef286fb38d';
     var appsecret = '39278936f9e35c2e82ed57f25a05717f';
 
+    // 3번 단말
+    // var appID = 'wx9242b0f3c507cb24';
+    // var appsecret = '3f901f86a389445276b13e1b661935d6';
+
+
     var accessTokenURL = "https://api.wechat.com/cgi-bin/token?grant_type=client_credential&appid="+appID+"&secret="+appsecret;
     var accessTokenOptions = {
         method: "GET",
@@ -293,11 +279,7 @@ function getOpenId(req ,res,next  )
 
                 req.body.openId = Array[0]["USER_OPEN_ID"];
                 //화면에서 주소 등록 인경우 처리
-                if (req.body.message =='1' ) {
-                    // req.body.openId = Array[0]["USER_OPEN_ID"];
-                    console.log(" Before!! RoadAddressInsert ");
-                    RoadAddressInsert(req,res,next);
-                }
+
                 if (req.body.message =='2') {
                     //오픈아디,메세지로 전송하기
                     getToken(req.body.openId, req.body.StartMessage);
@@ -326,12 +308,13 @@ function RoadAddressInsert(req, res, next  )
         //입력할 값 셋팅
         var road_info_set   =
             {
-                'USER_OPEN_ID'     :  RETURN_DATA.openId ,
+                'USER_WECHAT_ID'   : req.body.wechatId ,
                 'START_NM_CN'      : req.body.StartCn,
                 'START_NM_KR'      : req.body.StartKr,
                 'END_NM_CN'        : req.body.EndCn,
                 'END_NM_KR'        : req.body.EndKr
             };
+
         //insert 문장
         var query = 'INSERT INTO TB_ROAD_INFO SET  ?' ;
 
@@ -351,5 +334,100 @@ function RoadAddressInsert(req, res, next  )
 
     return true;
 }
+
+
+function RoadAddressUpdate(req, res, next  )
+{
+    console.log(" Start!! RoadAddressUpdate ");
+
+    getConnection(function (err, connection) {
+        if(JSON.stringify(req.body) == '{}') {
+            res.status(404);
+            res.json({
+                "success" : 0,
+                "message" : "Parameters missing"
+            });
+            return false;
+        }
+
+        //입력할 값 셋팅 update입력값과 insert 입력값 포맷이 다름 {}, [] 괄호도 다름
+        var road_info_set   =
+            [
+                req.body.EndCn,
+                req.body.EndKr,
+                req.body.LargeAddrCn,
+                req.body.roadSeq ,
+                req.body.wechatId
+            ];
+
+        //insert 문장
+        var query = ' UPDATE TB_ROAD_INFO ' +
+            ' SET  END_NM_CN      = ?, ' +
+            'END_NM_KR       = ?, ' +
+            'LARGE_ADDR_CN   = ?  ' +
+            ' WHERE  ROAD_SEQ     = ?  ' +
+            ' AND  USER_WECHAT_ID  = ?  ';
+
+
+        connection.query(query, road_info_set , function (err, rows) {
+            if (err) {
+                console.error("err : " + err);
+                throw err;
+            }else{
+                console.log(" UPDATE SUCESS " );
+                res.status(200).send('UPDATE Sucess');
+
+                return true;
+            }
+            connection.release();
+        });
+    });
+
+    return true;
+}
+
+
+function SelectMaxSeq(req ,res,next  )
+{
+
+    getConnection(function (err, connection) {
+
+        //위챗 아디로 max seq 가져오기
+        var query = ' SELECT A.ROAD_SEQ   ' +
+            'FROM  TB_ROAD_INFO  A ' +
+            'WHERE A.USER_WECHAT_ID = ?         ' +
+            'ORDER BY A.ROAD_SEQ   DESC        LIMIT 1';
+
+        var id    =  req.body.wechatId;
+
+
+        connection.query(query, id, function (err, rows) {
+            if (err) {
+                //  console.error("err : " + err);
+                throw err;
+            }else{
+                console.log("rows1 : " +  JSON.stringify(rows));
+
+                var Array = JSON.parse(JSON.stringify(rows ));
+
+                req.body.roadSeq = Array[0]["ROAD_SEQ"];
+                //화면에서 주소 등록 인경우 처리
+                if (req.body.message =='1' )
+                {
+                    console.log(" Before!! RoadAddress Update ");
+                    RoadAddressUpdate(req,res,next);
+                }
+
+            }
+
+            connection.release();
+        })
+    })
+
+
+    console.log("openID : " + RETURN_DATA.openId);
+    return true;
+}
+
 
 module.exports = router;
