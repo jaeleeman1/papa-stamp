6dc523843d9cd3b6d8ec331c2a8ebb20ca9061f1
jaeleeman1 <fromjaelee@naver.com>
2017-02-05 21:09:14 +0900

    Update shopping.js

--- routes/shopping.js
+++ routes/shopping.js
@@ -6,7 +6,8 @@ var config = require('../lib/config');
 //GET Shopping List
 router.get('/', function (req, res, next) {
     getConnection(function (err, connection){
-        var wechatId = 'couphone0001';//req.query.nick_name; // wechat Id
+        //var wechatId = 'couphone0001';//req.query.nick_name; // wechat Id
+	var wechatId = 'felix';//req.query.nick_name; // wechat Id
         var openId = '';
         var buyData = '';
     	var buyPrdctCnt;
