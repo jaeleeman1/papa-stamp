101204a9354e6bf3a76ffb6a0303efbd6b42cac0
Charles Chang <charles817373@gmail.com>
2017-02-09 14:46:20 +0900

    Enable console log about printSessionList

--- routes/responsor.js
+++ routes/responsor.js
@@ -583,11 +583,11 @@ function deleteUserInfo(open_id) {
 }
 
 function printSessionList() {
-    var agentName = "couphone0002";
+    var agentName = "couphone0004";
     getUserListOfAgent(agentName, function(err, result) {
         if(!err) {
-            // console.log("*** printSessionList("+agentName+") Success ***");
-            // console.log(result);
+            console.log("*** printSessionList("+agentName+") Success ***");
+            console.log(result);
         } else {
             console.log("*** printSessionList("+agentName+") Fail ***");
         }
