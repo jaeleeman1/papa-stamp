ef23a4ab63475778aef62f8b43ef87ffeecec1a7
felix <fromjaelee@naver.com>
2017-02-14 17:30:16 +0900

    init 메시지 수정

--- routes/responsor.js
+++ routes/responsor.js
@@ -466,7 +466,6 @@ function shoppingSendMessage(nick_name, open_id) {
             picurl : "https://s3.ap-northeast-2.amazonaws.com/cphone-storage/couphone_image/shopping_img.jpg"
         },
         {
-            url : shoppingInitUrl
         }
     ];
 
