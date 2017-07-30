21151f6381161e9c3aa880ef86e5849c8791ec20
jaeleeman1 <fromjaelee@naver.com>
2017-07-30 14:10:05 +0900

    로그인 로직 수정

--- routes/login.js
+++ routes/login.js
@@ -15,4 +15,25 @@ router.post('/tablet/mainPage', function(req, res, next) {
     res.render('tablet/tabletMain',{nickName: req.body.login_id, listLength : 0 });
 });
 
+/* GET user info */
+router.get('/userInfo', function(req, res, next) {
+    var userId = req.query.user_id;
+
+    getConnection(function (err, connection){
+        // Select User Infomation
+        var selectUserInfo = 'select USER_PASSWORD from SB_USER_INFO where USER_ID = ?';
+        connection.query(selectUserInfo, userId, function (err, row) {
+            if (err) {
+                console.error("[User Info Error] Select User Info Error : " + err);
+                throw err;
+            }else{
+                console.log("[User Info] Select User Info Success ### " + JSON.stringify(row));
+                res.status(200);
+                res.send(row[0].USER_PASSWORD);
+            }
+            connection.release();
+        });
+    });
+});
+
 module.exports = router;
