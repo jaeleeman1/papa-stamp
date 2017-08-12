babb61383b13bcf137a94abe665fd29bb00be0fc
jaeleeman1 <fromjaelee@naver.com>
2017-08-12 13:08:40 +0900

    config

--- config/config.js
+++ config/config.js
@@ -2,16 +2,16 @@ var enviromentConfig = require('config');
 var config = {};
 
 //Database
-config.host = enviromentConfig.get('local.host');
-// config.host = 'ipark.cd2ja5ctb3jo.ap-northeast-1.rds.amazonaws.com';
+// config.host = '127.0.0.1';
+config.host = 'ipark.cd2ja5ctb3jo.ap-northeast-1.rds.amazonaws.com';
 config.port = 43306;
 config.username = 'ipark';
 config.password = 'lee2336^^';
 config.database = 'BEST_FOOD';
 
 //Domain
-config.url = enviromentConfig.get('local.url');
-// config.url = 'https://whereareevent.com';
+// config.url = 'http://localhost:8080';
+config.url = 'https://whereareevent.com';
 
 //Logging
 config.loglevel = 'debug';
