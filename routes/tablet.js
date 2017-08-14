ad614b70f22f7462113669e2505de43e3741fdb3
jaeleeman1 <fromjaelee@naver.com>
2017-08-14 12:21:56 +0900

    socket.io 테스트

--- routes/tablet.js
+++ routes/tablet.js
@@ -1,8 +1,28 @@
 var express = require('express');
+var path = require('path');
+var http = require('http');
 var router = express.Router();
 var config = require('../config/service_config');
 var getConnection = require('../config/db_connection');
 var request = require('request');
+
+var app = express();
+app.use(express.static(path.join(__dirname, 'public')));
+
+var httpServer =http.createServer(app).listen(8060, function(req,res){
+    console.log('Socket IO server has been started');
+});
+
+var io = require('socket.io').listen(httpServer);
+
+io.sockets.on('connection',function(socket){
+    socket.emit('toclient',{msg:'Welcome !'});
+    socket.on('fromclient',function(data){
+        socket.broadcast.emit('toclient',data); // 자신을 제외하고 다른 클라이언트에게 보냄
+        socket.emit('toclient',data); // 해당 클라이언트에게만 보냄. 다른 클라이언트에 보낼려면?
+        console.log('Message from client :'+data.msg);
+    })
+});
 
 /* GET login (session) */
 router.get('/main', function(req, res, next) {
