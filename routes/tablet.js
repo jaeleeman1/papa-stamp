2ded3f987b86ef137dffcd8266f1e7a9a5f2402b
jaeleeman1 <fromjaelee@naver.com>
2017-08-15 14:56:51 +0900

    socket.io 테스트

--- routes/tablet.js
+++ routes/tablet.js
@@ -10,6 +10,7 @@ var request = require('request');
 var app = express();
 app.use(express.static(path.join(__dirname, 'public')));
 
+/*
 var options = {
     key: fs.readFileSync(path.join(process.cwd(),'public//ssl/key.pem')),
     cert: fs.readFileSync(path.join(process.cwd(),'public/ssl/cert.pem'))
@@ -20,7 +21,13 @@ var httpsServer =https.createServer(options, app).listen(8060, function(req,res)
 });
 
 var io = require('socket.io').listen(httpsServer);
+*/
 
+var httpServer =http.createServer(app).listen(8060, function(req,res){
+    console.log('Socket IO server has been started');
+});
+
+var io = require('socket.io').listen(httpServer);
 io.sockets.on('connection',function(socket){
     socket.on('fromclient',function(data){
         socket.broadcast.emit('toclient',data); // 자신을 제외하고 다른 클라이언트에게 보냄
@@ -31,10 +38,9 @@ io.sockets.on('connection',function(socket){
 
 /* GET login (session) */
 router.get('/pushStamp', function(req, res, next) {
-    io.sockets.on('connection',function(socket){
-        socket.emit('toclient',data); // 해당 클라이언트에게만 보냄. 다른 클라이언트에 보낼려면?
-    });
+    io.sockets.emit('toclient',{sendData: "send success"}); // 해당 클라이언트에게만 보냄. 다른 클라이언트에 보낼려면?
     res.status(200);
+    res.send("success");
 });
 
 /* GET login (session) */
