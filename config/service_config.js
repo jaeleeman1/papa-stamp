6a798ad462cadfce1d4b678961f12dbabcbe4cdc
jaeleeman1 <fromjaelee@naver.com>
2017-10-09 17:52:47 +0900

    key 추가

--- config/service_config.js
+++ config/service_config.js
@@ -2,18 +2,24 @@
 var config = {};
 
 //Database
-// config.host = '127.0.0.1';
-config.host = 'ipark.cd2ja5ctb3jo.ap-northeast-1.rds.amazonaws.com';
+config.host = '127.0.0.1';
+// config.host = 'ipark.cd2ja5ctb3jo.ap-northeast-1.rds.amazonaws.com';
 config.port = 43306;
 config.username = 'ipark';
 config.password = 'lee2336^^';
 config.database = 'BEST_FOOD';
 
 //Domain
-// config.url = 'http://localhost:8080';
-config.url = 'https://whereareevent.com';
+config.url = 'http://localhost:8080';
+// config.url = 'https://whereareevent.com';
 
 //Logging
 config.loglevel = 'debug';
+
+//Firebase
+config.serverKey = 'AAAAHIVXzfk:APA91bHqH863OCv5t6oNHwoYjDp5kmqd-D6GtrrU-QW_ikVCkW2HteP6pnvCT58XhKH4bobu0jOPZyzF2w1DFE1z4ktQ1bVS59iXQi70qqGFyW8g9LNLR8KgksXrm9lzQ1_FVsDsQZt0';
+
+//AES Key
+config.secrectKey = 'Glu0r6o0GzBZIe0Qsrh2FA==';
 
 module.exports = config;
