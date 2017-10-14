244719f1edf0fdd5976ac1d80700addd9fa77b86
jaeleeman1 <fromjaelee@naver.com>
2017-10-14 22:02:33 +0900

    download url

--- routes/download.js
+++ routes/download.js
@@ -1,20 +1,11 @@
 var express = require('express');
 var router = express.Router();
-//var path = require('path');
-//var mime = require('mime');
 
-const TAG = 'Router: DownloadManager:';
+const TAG = '[DOWNLOAD] ';
 
-router.get('/ssup.apk', function(req, res, next) {
-    var filepath = __dirname + '/../download/ssup-v0.1.12.apk'
-    //console.log(TAG, '__dirname: ' + __dirname);
+router.get('/papastamp.apk', function(req, res, next) {
+    var filepath = __dirname + '/../download/papastamp-v1.0.0.apk'
     console.log(TAG, 'filepath: ' + filepath);
-
-    //var filename = path.basename(filepath);
-    //var mimetype = mime.lookup(filepath);
-    //console.log(TAG, 'filename: ' + filename);
-    //console.log(TAG, 'mimetype: ' + mimetype);
-
     res.download(filepath);
 });
 
