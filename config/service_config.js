d0082061631c54c1eb5c415a111e17ac25f313b0
jaeleeman1 <fromjaelee@naver.com>
2017-11-22 23:29:19 +0900

    db 변경

--- config/service_config.js
+++ config/service_config.js
@@ -2,8 +2,8 @@
 var config = {};
 
 //Database
-// config.host = '127.0.0.1';
-config.host = 'ipark.cd2ja5ctb3jo.ap-northeast-1.rds.amazonaws.com';
+//config.host = '127.0.0.1';
+config.host = 'papastamp.cgbapsrwofhr.ap-northeast-1.rds.amazonaws.com';
 config.port = 43306;
 config.username = 'ipark';
 config.password = 'lee2336^^';
